2e1bf090469b16752857192471a12614b5dfa947
Yasin <yasin@limunis.com>
2021-01-05 00:46:22 +0200

    Proje dosyası ekle.

---
+++ src/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shiptoboxes.WebMvc.Controllers
+{
+    public class AdminController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ src/Controllers/LogisticController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shiptoboxes.WebMvc.Controllers
+{
+    public class LogisticController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
